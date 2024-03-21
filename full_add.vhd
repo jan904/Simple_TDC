45a73817f63bb4b78db2984940e9edeaf1f3ed1a
Jan Höfer <153934511+jan904@users.noreply.github.com>
2024-03-21 10:06:25 +0100

    Update full_add.vhd

--- full_add.vhd
+++ full_add.vhd
@@ -1,3 +1,15 @@
+-- Full adder fo two 1 Bit inputs
+--
+-- This is a full adder that takes two 1 Bit inputs and a carry in and outputs a carry out.
+-- The sum is not calculated as we don't need it for the purpose of a delay chain.
+--
+--Inputs:
+--  a, b : 1 Bit inputs
+--  Cin : Carry in
+--
+--Outputs:
+--  Cout : Carry out
+
 library ieee;
 use ieee.std_logic_1164.all;
 
